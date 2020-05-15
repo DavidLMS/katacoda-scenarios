88b6f08dd658d7e2d1e2734c499f2054feff7800
DavidLMS <lms.davidromero@gmail.com>
2020-05-15 18:19:21 +0200

    Update inicio.sh

--- ftp/inicio.sh
+++ ftp/inicio.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 apt update
+wait 5
 apt install nginx -y
+wait 5
 apt install php -y
+wait 5
 cd /var/www/html/
 wget https://github.com/DavidLMS/katacoda-scenarios/raw/master/ftp/assets/monsta_ftp_2.10.1_install.zip
+wait 3
 unzip monsta_ftp_2.10.1_install.zip
 cp -R mftp/* ./
 rm -R mftp
