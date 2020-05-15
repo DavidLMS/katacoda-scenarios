a3eb1879a3215bf642c45537832375078b015d33
DavidLMS <lms.davidromero@gmail.com>
2020-05-15 18:26:17 +0200

    Update inicio.sh

--- ftp/inicio.sh
+++ ftp/inicio.sh
@@ -2,9 +2,8 @@
 nohup apt update
 nohup apt install nginx -y
 nohup apt install php -y
-nohup cd /var/www/html/
-nohup wget https://github.com/DavidLMS/katacoda-scenarios/raw/master/ftp/assets/monsta_ftp_2.10.1_install.zip
-nohup unzip monsta_ftp_2.10.1_install.zip
-nohup cp -R mftp/* ./
-nohup rm -R mftp
-nohup rm monsta_ftp_2.10.1_install.zip
+nohup wget -P /var/www/html/ https://github.com/DavidLMS/katacoda-scenarios/raw/master/ftp/assets/monsta_ftp_2.10.1_install.zip
+nohup unzip /var/www/html/monsta_ftp_2.10.1_install.zip
+nohup cp -R /var/www/html/mftp/* /var/www/html/
+nohup rm -R /var/www/html/mftp
+nohup rm /var/www/html/monsta_ftp_2.10.1_install.zip
