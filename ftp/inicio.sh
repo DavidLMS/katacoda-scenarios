b8301070832eed055087e1c6b50ba5007f865841
DavidLMS <lms.davidromero@gmail.com>
2020-05-15 18:40:06 +0200

    Actualizado script de inicio

--- ftp/inicio.sh
+++ ftp/inicio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nohup apt update
 nohup apt install nginx -y
-nohup apt install php -y
 nohup wget -P /var/www/html/ https://github.com/DavidLMS/katacoda-scenarios/raw/master/ftp/assets/monsta_ftp_2.10.1_install.zip
+nohup apt install php -y
 nohup unzip /var/www/html/monsta_ftp_2.10.1_install.zip
