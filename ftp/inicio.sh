0e6a994be57971aa6e6d3f22d80f009779c92c05
DavidLMS <lms.davidromero@gmail.com>
2020-05-16 10:26:46 +0200

    Arreglado inicio

--- ftp/inicio.sh
+++ ftp/inicio.sh
@@ -2,10 +2,9 @@
 nohup apt update
 nohup apt install nginx -y
 nohup wget -P /var/www/html/ https://github.com/DavidLMS/katacoda-scenarios/raw/master/ftp/assets/monsta_ftp_2.10.1_install.zip
-echo "done" >> /root/nginx-terminado
 nohup apt install php -y
 nohup rm /etc/nginx/sites-available/default
 nohup wget -P /etc/nginx/sites-available https://github.com/DavidLMS/katacoda-scenarios/raw/master/ftp/assets/default
 nohup unzip /var/www/html/monsta_ftp_2.10.1_install.zip -d /var/www/html
 nohup service nginx restart
-echo "done" >> /root/monsta-ftp-terminado
+echo "done" >> /opt/.backgroundfinished
