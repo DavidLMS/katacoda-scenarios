be7901a068d6b6044851c6ea6864accd40d52914
DavidLMS <lms.davidromero@gmail.com>
2020-05-16 10:28:53 +0200

    Update espera.sh

--- ftp/assets/espera.sh
+++ ftp/assets/espera.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Espera a que el sistema esté configurado completamente antes de ejecutar un comando..."; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Hecho"
+echo "Espera a que el sistema esté configurado completamente antes de ejecutar algún comando..."; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Listo"
